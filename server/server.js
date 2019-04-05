87b6c4d36471c34c6c00acb7717f64f171594d42
sacullezzar <lucas.razzell@holidayextras.com>
2019-04-05 11:34:15 +0100

    connected to mLab and created Routes

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const PORT = 14000
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use('./todos', require('./routes/routes'))
+
+mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds231956.mlab.com:31956/todo`, {
+    useNewUrlParser: true 
+})
+const connection = mongoose.connection
+
+connection.once('open', () => {
+    console.log("MongoDB connected to Database!")
+})
+
+app.listen(PORT, () => {
+    console.log(process.env.DB_USER, process.env.DB_PASS)
+    console.log(`Db server running on Port: ${PORT}`)
+})
